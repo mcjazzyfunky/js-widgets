edaa3e5e471d1ad8f8039947b2ea1ab9035b50ca
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2019-04-01 21:17:41 +0200

    Some updates

--- src/modules/util/main/api/prepareView.ts
+++ src/modules/util/main/api/prepareView.ts
@@ -3,10 +3,10 @@ import { VirtualNode } from '../../../core/main/index'
 type Props = Record<string, any>
 type Renderer<P extends Props> = ((props: P) => VirtualNode)
 
-export default function prepareView<P extends Props>(actionBefore: () => void) {
+export default function prepareView<P extends Props>(actionBefore: (props: P) => void) {
   return (renderer: Renderer<P>) => {
     return (props: P) => {
-      actionBefore()
+      actionBefore(props)
       return renderer(props)
     }
   }
