fe8edb1e9bac843a3cb397e287e458a5eabe2b20
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2019-09-16 16:37:23 +0200

    Some updates

--- src/modules/core/main/internal/types/StatefulComponentMeta.ts
+++ src/modules/core/main/internal/types/StatefulComponentMeta.ts
@@ -1,6 +1,6 @@
-import Ctrl from './Ctrl'
-import Props from './Props'
-import VirtualNode from './VirtualNode'
+import Ctrl from '../../api/types/Ctrl'
+import Props from '../../api/types/Props'
+import VirtualNode from '../../api/types/VirtualNode'
 
 type StatefulComponentMeta<P extends Props = {}> = {
   displayName: string,
