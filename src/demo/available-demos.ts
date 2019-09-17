8be62175712798f47bc079c99c624b3609f8ef1b
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2019-09-17 13:47:50 +0200

    Some updates

--- src/demo/available-demos.ts
+++ src/demo/available-demos.ts
@@ -10,6 +10,7 @@ import boundary from './demos/boundary'
 import stopWatch1 from './demos/stop-watch-1'
 import stopWatch2 from './demos/stop-watch-2'
 import mousePosition from './demos/mouse-position'
+import context from './demos/context'
 import i18n from './demos/i18n'
 import performanceTest1 from './demos/performance-test-1'
 import performanceTest2 from './demos/performance-test-2'
@@ -25,6 +26,7 @@ const demos: [string, VirtualElement][] = [
   ['Stop watch 1', stopWatch1],
   ['Stop watch 2', stopWatch2],
   ['Mouse position', mousePosition],
+  ['Context', context],
   ['Internationalization', i18n],
   ['Performance test 1', performanceTest1],
   ['Performance test 2', performanceTest2],
